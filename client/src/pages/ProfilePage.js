e05dbc6ceb1102367de9e7911cd87a44c90a0afc
ollipc <ollijk133@gmail.com>
2024-03-11 08:14:51 -0700

    removed unneccessary useNavigation

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {ProfileForm} from '../components/ProfileForm';
-import {useNavigate} from "react-router-dom"
+
 const ProfilePage = () => {
-  const navigate = useNavigate();
 
   return (
     <div>
