c5ba2fd5c5fd04a5f16cd084e24aa2df4280c315
Mapko <Marko.Karttunen@edu.savonia.fi>
2024-03-11 10:12:18 +0200

    Branch mergerys

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -15,7 +15,7 @@ router.post('/add', async (req, res) => {
   });
 
   router.post('/search', async (req, res) => {
-    if(!requestUtils.isJson) {
+    if(!isJson) {
       //If it wasn't, the responseUtils.badRequest function is returned, which takes res and an error message as parameters.
       return badRequest(res, "Content was not Json");
     }
