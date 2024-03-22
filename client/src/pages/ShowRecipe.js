8d1aa6b9411b28c8fed644189b4b465e9549360b
MiksiMankeli <mikko.h.pentikainen@edu.savonia.fi>
2024-03-22 13:02:56 +0200

    ShowRecipe tyylittelyjä (kesken)

--- client/src/pages/ShowRecipe.js
+++ client/src/pages/ShowRecipe.js
@@ -65,48 +65,46 @@ const ShowRecipe = () => {
 
  
   return (
-    <div>
-      <h1>Recipe</h1>
+    <div className='app'>
       {searchResults.length > 0 && (
         <div>
           {searchResults.map((recipe) => (
-            <div key={recipe.recipeid} style={{ display: 'flex', marginBottom: '20px' }}>
-              {recipe.images && (
-                <div style={{ flex: 1, marginRight: '20px' }}>
-                  <strong>Image:</strong><br />
-                  {/* Convert Buffer object to base64 encoded string */}
-                  <img src={`data:image/jpeg;base64,${arrayBufferToBase64(recipe.images.data)}`} alt="Recipe Image" style={{ maxWidth: '300px' }} />
+            <div key={recipe.recipeid}>
+              <div className='compartment-container'>
+                <div className='compartment1'>
+                  {recipe.images && (
+                    <div>
+                      <h1>{recipe.recipename}</h1>
+                      {/* Convert Buffer object to base64 encoded string */}
+                      <img src={`data:image/jpeg;base64,${arrayBufferToBase64(recipe.images.data)}`} alt="Recipe Image" style={{ maxWidth: '300px' }} />
+                    </div>
+                  )}
                 </div>
-              )}
-              <div style={{ flex: 2 }}>
-                <p><strong>Recipe ID:</strong> {recipe.recipeid}</p>
-                <p><strong>User ID:</strong> {recipe.userid}</p>
-                <p><strong>Recipe Name:</strong> {recipe.recipename}</p>
-                <p><strong>Category:</strong> {recipe.category}</p>
-                <p><strong>Description:</strong> {recipe.description}</p>
-                <p><strong>Tags:</strong> {recipe.tags}</p>
-                <p><strong>Instructions:</strong> {recipe.instructions}</p>
-
+                <div className='compartment2'>
+                  <p><strong>Category:</strong> {recipe.category}</p>
+                  <p><strong>Description:</strong> {recipe.description}</p>
+                  <p><strong>Tags:</strong> {recipe.tags}</p>
+                  <p><strong>Instructions:</strong> {recipe.instructions}</p>
+                </div>  
+              </div>
+              <div className='compartment3'>
                 {!hasReviewed && <RatingComponent userid={user.userid} recipeid={recipe.recipeid} />}
                 
-
                 {recipeReviews.map(review => (
                   <div key={review.reviewid}>
                     <p>User ID: {review.userid}</p>
                     <p>Rating: {review.rating}</p>
                     {/* You can render more details of the review here */}
                   </div>
-                ))}
-                
-              </div>
-
-              
+                ))}               
+              </div>           
             </div>
           ))}
         </div>
       )}
     </div>
   );
+  
 };
 
 // Function to convert ArrayBuffer to base64
