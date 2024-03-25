01ff54c79a441bc89a29efe1bc192ede6117bcaf
ollipc <ollijk133@gmail.com>
2024-03-25 09:13:15 -0700

    homepage changes

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,10 +1,38 @@
+import React, { useState, useEffect } from 'react';
 const HomePage = () => {
+    const [input, setInput] = useState('');
+    const searchRecipes = async () => {
+        try {
+            console.log("lol")
+            const response = await fetch("http://localhost:3001/api/recipe", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ input }),
+            });
+            const data = await response.json();
+            if (!response.ok) {
+              throw new Error(data.error);
+            } 
+            
+            if (response.ok) {
+                console.log(data)
+            } 
+          } catch (error) {
+            console.error("Error:", error);
+          }
+    }
+
+
+
+
     return(
         <div className="container-home">
             <div className="homepage-top">
                 <h1>Resepti haku</h1>
                 <div className="homepage-search">
-                    <input type="text" className="home-search-input" placeholder="Etsi..."></input><button className="home-search-button">Hae</button>
+                    <input type="text" className="home-search-input" placeholder="Etsi..." onChange={(e) => setInput(e.target.value)}></input><button onClick={() => searchRecipes()} className="home-search-button">Hae</button>
                 </div>
             </div>
         </div>
